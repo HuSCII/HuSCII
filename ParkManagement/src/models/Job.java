1873f063b83abb63989dfb1e5f2a7a38406fef6d
Ian McPeek <iansmcpeek@gmail.com>
2015-05-02 17:43:18 -0700

    -create package 'tests' to put our JUnit classes -put files in new package 'models' -added jobFile and filled with dummy data -JobController class is more stubbed out

--- ParkManagement/src/models/Job.java
+++ ParkManagement/src/models/Job.java
@@ -1,3 +1,4 @@
+package models;
 import java.util.GregorianCalendar;
 import java.util.Map;
 
@@ -7,7 +8,7 @@ public class Job {
 	private String parkName;
 	private String jobName;
 	private GregorianCalendar date;
-	private int jobDuration;
+	private int jobDuration; //in hours
 	private int currentLight;
 	private int maxLight;
 	private int currentMedium;
@@ -19,9 +20,13 @@ public class Job {
 	//init still incomplete - Ian
 	Job(final String parkName, final String jobName, 
 			final String date, final int jobDuration) {
+		//should a job check if its allowed to be created,
+		// i.e. ask jobController if the max jobs is reached
+		//or should job controller takes in the data and create
+		//a job only if max isn't met? -Ian
 		this.parkName = parkName;
 		this.jobName = jobName;
-		//parse formatted string 'date'
+		//parse formatted string 'date' format:"m/d/yyyy hh:mmAM" see jobFile
 		this.date = new GregorianCalendar(year, month, 
 				dayOfMonth, hourOfDay, minute);
 	}
--- ParkManagement/src/models/Volunteer.java
+++ ParkManagement/src/models/Volunteer.java
@@ -1,3 +1,4 @@
+package models;
 public class Volunteer {
 	private int vounteerID;
 	private String lastName;
