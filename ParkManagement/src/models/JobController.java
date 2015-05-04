fd8dc9e1a8e4f3f5723c01dbe823beb1d524aa43
Ian McPeek <iansmcpeek@gmail.com>
2015-05-03 17:27:03 -0700

    -filled in loadJobData

--- ParkManagement/src/models/JobController.java
+++ ParkManagement/src/models/JobController.java
@@ -1,5 +1,14 @@
 package models;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import models.Job.WorkCatagories;
 
 
 public class JobController {
@@ -7,17 +16,16 @@ public class JobController {
 	private List<Job> allJobs;
 	
 	JobController() {
-		
+		allJobs = new ArrayList<Job>();
 	}
 	//*****PUBLIC METHODS*****//
-	public boolean addJob(Job job) {
-		//should I take in a job or the info of a 
-		//job to create one?
-		return false;
+	public void addJob(Job job) {
+		allJobs.add(job);
 	}
 	
 	public List<Job> getUpcomingJobs() {
 		return null;
+		//job needs an isComplete.
 	}
 	
 	public List<Job> getAllJobs() {
@@ -27,6 +35,49 @@ public class JobController {
 	
 	//*****PRIVATE METHODS*****//
 	private boolean loadJobData() {
+		String fileloc = "jobFile.txt";
+		File file = new File(fileloc);
+		try {
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNext()) {
+				StringTokenizer token = new StringTokenizer(scanner.nextLine(), ",");
+				
+				String parkName = token.nextToken();
+				String jobName = token.nextToken();
+				String date = token.nextToken();
+				int duration = Integer.parseInt(token.nextToken());
+				int currentLight = Integer.parseInt(token.nextToken());
+				int maxLight = Integer.parseInt(token.nextToken());
+				int currentMedium = Integer.parseInt(token.nextToken());
+				int maxMedium = Integer.parseInt(token.nextToken());
+				int currentHard = Integer.parseInt(token.nextToken());
+				int maxHard = Integer.parseInt(token.nextToken());
+				Map signedVolunteers = new HashMap<String, WorkCatagories>();;
+				//collect information
+				int i = 0;
+				while(token.hasMoreTokens()) {
+					//loop for emails
+					if(i>=currentLight) {
+						if(i>=currentLight+currentMedium) {
+							signedVolunteers.put(token.nextToken(), WorkCatagories.HEAVY);
+						} else {
+							signedVolunteers.put(token.nextToken(), WorkCatagories.MEDIUM);
+						}
+					} else {
+						signedVolunteers.put(token.nextToken(), WorkCatagories.LIGHT);
+					}
+					i++;
+				}
+				//create job
+				Job job = new Job(parkName, jobName, date, duration);
+				//add job
+				addJob(job);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
 		return true;
 	}
 	
