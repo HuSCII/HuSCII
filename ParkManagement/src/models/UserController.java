50c0cf07997e37c20258fb3d369e895357a50c98
Duy Huynh <dhuynh88@uw.edu>
2015-05-03 19:07:07 -0700

    UserController can write out a csv file of its User List.

--- ParkManagement/src/models/UserController.java
+++ ParkManagement/src/models/UserController.java
@@ -7,6 +7,8 @@ package models;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -24,28 +26,27 @@ public class UserController {
 
 	final List<User> userList = new ArrayList<User>();
 
-	public UserController() {
-
-		/*
-		 * Upon creation of this class, it should read in a text file and
-		 * populate a list of users
-		 */
-	}
-
-	public void readUserFile() {
+	/**
+	 * Parses a text file, creating a User for each line
+	 * 
+	 * @param inputFile
+	 *            Text file of user data.
+	 */
+	public void readUserFile(final String inputFile) {
 
 		try {
-			URL url = UserController.class.getResource("/userFile.csv");
+			URL url = UserController.class.getResource(inputFile);
 			File userFile = new File(url.toURI());
 			final Scanner fileInput = new Scanner(userFile);
 
+			// For each line of text, split it up using "," as delimeter
 			while (fileInput.hasNext()) {
-				List<String> userData = Arrays.asList(fileInput.nextLine()
-						.split(","));
+				final List<String> userData = Arrays.asList(fileInput
+						.nextLine().split(","));
 
+				// Add each User to the List
 				userList.add(new User(userData.get(0), userData.get(1),
 						userData.get(2), userData.get(3)));
-
 			}
 
 		} catch (FileNotFoundException | URISyntaxException e) {
@@ -54,21 +55,32 @@ public class UserController {
 
 	}
 
-	public void writeUserFile() {
+	/** Write List of Users into textfile. */
+	public void writeUserFile(final String outputFile) {
 
+		try {
+			FileWriter writer = new FileWriter(outputFile);
+			writer.append(toString());
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public List getUserList() {
+	/** Returns the Users in a List. */
+	public List<User> getUserList() {
 		return userList;
 	}
 
 	/** String representation of users and their data. */
 	public String toString() {
+
 		final StringBuilder sb = new StringBuilder();
 
 		// Append each user line
 		for (User u : userList) {
-			sb.append(u.toString() + "\n");
+			sb.append(u.toString() + "\r\n");
 		}
 
 		return sb.toString();
