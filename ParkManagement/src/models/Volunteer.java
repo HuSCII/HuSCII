8e820bbfa9469a56b37cdc7b8ddd66fea3f27122
Putthida Samrith <psamrith@uw.edu>
2015-05-04 20:04:20 -0700

    add work category parameter to signUp method

--- ParkManagement/src/models/Volunteer.java
+++ ParkManagement/src/models/Volunteer.java
@@ -1,13 +1,15 @@
 package models;
 
+import models.Job.WorkCatagories;
+
 public class Volunteer extends User {
 
 	protected Volunteer(String email, String firstName, String lastName, String role) {
 		super(email, firstName, lastName,role );
 	}
 
-	public void signUp(Job parkJob) {
-		parkJob.addVolunteer(getEmail(), null);
+	public void signUp(Job parkJob, WorkCatagories workCat) {
+		parkJob.addVolunteer(getEmail(), workCat);
 	}
 
 	public void findSignUpJobs() {
