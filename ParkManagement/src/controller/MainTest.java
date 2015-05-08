41b132473794ad038e2d730fc5a4217107b75107
duyh <dhuynh88@uw.edu>
2015-05-08 12:10:35 -0700

    Main console controller can find the user if given the email address and correctly identify the role!

--- ParkManagement/src/controller/MainTest.java
+++ ParkManagement/src/controller/MainTest.java
@@ -3,19 +3,23 @@ package controller;
 import java.util.Scanner;
 
 import models.JobController;
+import models.User;
 import models.UserController;
 
 public class MainTest {
 
+	static UserController userController;
+	static JobController jobController;
+
 	public static void main(String[] args) {
 
 		// Pre-load the User persistent data:
-		UserController userController = new UserController();
-		userController.readUserFile("/userFile.csv");
+		userController = new UserController();
+		userController.readUserFile("/testFile.csv");
 
 		// Pre-load the Jobs persistent data:
 		// instantiate a jobcontroller
-		JobController jobController = new JobController("/jobFile.txt");
+		jobController = new JobController();
 		// read in the jobs text file
 
 		// Display doc
@@ -23,6 +27,7 @@ public class MainTest {
 
 		// Display login prompt:
 		String email = retrieveEmail();
+		verifyEmail(email);
 
 		// assign
 
@@ -51,6 +56,19 @@ public class MainTest {
 		System.out.println("Please login with your email address:");
 		Scanner input = new Scanner(System.in);
 		return input.nextLine();
+	}
+
+	private static boolean verifyEmail(String email) {
+
+		for (User u : userController.getUserList()) {
+			if (email.equals(u.getEmail())) {
+				System.out.println("You are a registered user!");
+				System.out.println(u.getRole());
+				return true;
+			}
+		}
+		System.out.println("Email not found!");
+		return false;
 	}
 
 	private static void assignRole(String email) {
