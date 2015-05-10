564ef7314578f89073905572bd476faa2c22958e
Ian McPeek <iansmcpeek@gmail.com>
2015-05-10 11:06:05 -0700

    -added VolunteerController class to controller package

---
+++ ParkManagement/src/controller/VolunteerController.java
@@ -0,0 +1,20 @@
+package controller;
+
+public class VolunteerController {
+	
+	
+	public static void volunteerStart() {
+		System.out.println("Welcome back Volunteer!");
+		System.out.println("Select an option from the list below.");
+		
+		System.out.println("1. View all upcoming jobs.");
+		System.out.println("2. View jobs volunteer has signed up for.");
+		System.out.println("0. Exit.");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
