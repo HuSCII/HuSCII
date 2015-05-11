39531e350eacaf19400ce523f940c2e93d5a6ca7
Jingzhu Guo <judeguo8@uw.edu>
2015-05-11 00:15:06 -0700

    modified the park manager Junit test

--- ParkManagement/src/tests/ParkManagerTest.java
+++ ParkManagement/src/tests/ParkManagerTest.java
@@ -1,58 +1,41 @@
 package tests;
 
 import static org.junit.Assert.*;
-import models.ParkManager;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test class for ParkManager class
- * @author Jingzhu Guo
- *
- */
 public class ParkManagerTest {
 
-	@Before
-	public void setUp() throws Exception {
-		
-		ParkManager manager = new ParkManager("abc@gmail.com", "John", "Wang", "Park Manager");
-		manager.addPark("ALkI");
-	}
+    @Before
+    public void setUp() throws Exception {
+        
+        
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testParkManager() {
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testParkManager() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testRetrieveManagedParks() {
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testAddPark(ParkManager manager) {
-		assertEquals ("same park name","ALKI", manager.getParks() );
-	}
+    @Test
+    public void testAddJob() {
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testCreateJob() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFindMyJob() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFindVolunteer() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetMyJobs() {
+        fail("Not yet implemented");
+    }
 
 }
