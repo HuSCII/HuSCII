44a7fff1d8d4e19720b04648ae853bded48d0580
Duy Huynh <dhuynh88@uw.edu>
2015-05-11 06:06:13 -0700

    Finished two of the USer stories in VOlunteerConsole. Need signup still.

--- ParkManagement/src/controller/VolunteerConsole.java
+++ ParkManagement/src/controller/VolunteerConsole.java
@@ -1,6 +1,7 @@
 
 package controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -21,8 +22,10 @@ public class VolunteerConsole {
 
     public VolunteerConsole(User user, JobController jobController) {
 
-        volunteer = new Volunteer(user.getEmail(), user.getFirstName(),
-                                  user.getLastName(), user.getRole());
+        volunteer =
+                        new Volunteer(user.getEmail(), user.getFirstName(),
+                                      user.getLastName(), user.getRole());
+        VolunteerConsole.jobController = jobController;
 
     }
 
@@ -33,10 +36,9 @@ public class VolunteerConsole {
         System.out.println("------------------------------");
         System.out.println("1. View all upcoming jobs.");
         System.out.println("2. View jobs I have signed up for.");
-        System.out.println("3. Sign up for a job.");
-        System.out.println("4. Logout");
-        System.out.println("5. Exit");
-        System.out.println("Please select menu choice 1-5: ");
+        System.out.println("3. Logout");
+        System.out.println("4. Exit");
+        System.out.print("Please select menu choice 1-5: ");
 
         final int menuSelect = keyboard.nextInt();
 
@@ -47,14 +49,11 @@ public class VolunteerConsole {
             case 2:
                 viewSignedUpJobs();
                 break;
-            case 3:
-                signMeUp();
-                break;
-            case 4: // Logout
+            case 3: // Logout
                 final String[] args = {};
                 MainConsole.main(args);
                 break;
-            case 5: // Exit
+            case 4: // Exit
                 break;
             default:
                 System.out.println("Not in a menu choice");
@@ -63,55 +62,76 @@ public class VolunteerConsole {
                 break;
         }
 
+        displayMenu(); // Display menu when done:
+
     }
-    
-    //    private static void listMyJobs() {
-    //        for (Job job: jobController.getUpcomingJobs()) {
-    //            System.out.println(job.toString());
-    //        }
-    //    }
-    
+
+    // private static void listMyJobs() {
+    // for (Job job: jobController.getUpcomingJobs()) {
+    // System.out.println(job.toString());
+    // }
+    // }
+
     public static void viewUpcomingJobs() {
 
         System.out.println("View available upcoming jobs:");
-        System.out.println("");
-
-//        System.out.println(volunteer.getFirstName() + " " + 
-//                        volunteer.getLastName() +" :" + volunteer.getEmail());
-        
-        //jobController.loadJobData("src/jobFile.txt");
+        System.out.println();
 
         final List<Job> myJobs = new ArrayList<Job>();
 
-                for (Job job : getMyJobs(jobController)) {
-                    myJobs.add(job);
-                    System.out.println(job.getParkName());
-                    System.out.println(job.getJobName());
-                }
+        int i = 1;
+        for (Job j : jobController.getUpcomingJobs()) {
+            System.out.print(i++ + ") ");
+            System.out.println(j.getParkName());
+            System.out.println(j.getJobName());
+            System.out.println("Start date & time: "
+                               + new SimpleDateFormat("MM/dd/yyyy HH:mm a").format(j.getDate()
+                                               .getTime()));
+            System.out.println("Duration: " + j.getJobDuration() + " hours");
+            System.out.println(j.getCurrentLight() + " out of " + j.getMaxLight()
+                               + " light-duty volunteers.");
+            System.out.println(j.getCurrentMedium() + " out of " + j.getMaxMedium()
+                               + " medium-duty volunteers.");
+            System.out.println(j.getCurrentHard() + " out of " + j.getMaxHard()
+                               + " heavy-duty volunteers.");
+            System.out.println();
+        }
+        System.out.println();
 
         // System.out.println(volunteer.getMyJobs(jobController));
     }
-    
-    public static List<Job> getMyJobs(JobController jobController) {
-        
-        final List<Job> volunteerJob = new ArrayList<Job>();
+
+    public static List<Job> getMyJobs() {
+
+        final List<Job> volunteersJob = new ArrayList<Job>();
 
         for (Job j : jobController.getUpcomingJobs()) {
-            if (volunteer.getEmail().equals(j.getVolunteerEmails())) {
-                volunteerJob.add(j);
+            for (String vol : j.getVolunteerEmails()) {
+                if (volunteer.getEmail().equals(vol)) {
+                    volunteersJob.add(j);
+                }
             }
         }
 
-        return volunteerJob;
+        return volunteersJob;
 
     }
 
     public static void viewSignedUpJobs() {
 
         System.out.println("Viewing the jobs you have signed up for:");
-        System.out.println("");
 
-        volunteer.findSignUpJobs();
+        for (Job j : jobController.getUpcomingJobs()) {
+            for (String vol : j.getVolunteerEmails()) {
+                if (volunteer.getEmail().equals(vol)) {
+                    System.out.print(j.getJobName() + " at ");
+                    System.out.print(j.getParkName() + " on ");
+                    System.out.println(new SimpleDateFormat("MM/dd/yyyy HH:mm a").format(j
+                                    .getDate().getTime()));
+                    System.out.println();
+                }
+            }
+        }
 
     }
 
@@ -119,7 +139,6 @@ public class VolunteerConsole {
 
         System.out.println("Sign up for this job:");
         keyboard = new Scanner(System.in);
-
 
         System.out.print("Enter a Job Name (ie trash pickup): ");
         String jobName = keyboard.nextLine();
