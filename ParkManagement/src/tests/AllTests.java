455983f26b4ff8324938683dc75a431895e369e3
Putthida Samrith <psamrith@uw.edu>
2015-05-18 03:07:25 -0700

    add JUnit test suite

---
+++ ParkManagement/src/tests/AllTests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * This is a JUnit Test Suite for all JUnit test cases.
+ */
+@RunWith(Suite.class)
+@SuiteClasses({BusinessRulesTest.class, JobControllerTester.class, JobTest.class,
+    ParkManagerTest.class, UserControllerTest.class})
+public class AllTests {
+    //Everything can be done by using annotation above.
+}
